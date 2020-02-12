078b996d1111281815572705d3b4bdd50266814f
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2020-02-12 18:42:45 -0500

    hello -> printing

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,6 +11,8 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print('x is %(integer)i, y is %(float)f, z is "%(string)s"' %
+      {"integer": x, "float": y, "string": z})
 
 # Use the 'format' string method to print the same thing
 
