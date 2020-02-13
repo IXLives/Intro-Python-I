5efb66760e410da8ee10461792b6d9c2cd119ba5
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2020-02-12 20:36:06 -0500

    comprehensions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -43,11 +43,9 @@ print(y)
 # the user entered into list x.
 
 x = input("Enter comma-separated numbers: ").split(',')
-for num in x:
-    if int(num) % 2 == 0:
-        print(num)
+
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [num for num in x if int(num) % 2 == 0]
 
 print(y)
